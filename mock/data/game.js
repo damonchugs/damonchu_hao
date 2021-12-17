927f0a65a30fa802873e820c98998e0494463927
damonchugs <damonchugs@foxmail.com>
2021-12-17 15:27:04 +0800

    添加新数据

---
+++ mock/data/game.js
@@ -0,0 +1,45 @@
+const bbs_tab = [
+  {
+    name: '游戏论坛',
+    src: 'gamebbs',
+    hao: [
+      { name: '游民星空', href: 'https://www.gamersky.com/', description: '国内单机游戏门户网站,提供特色的“游戏资讯”,大量游戏攻略,经验,评测文章,以及热门游戏资料专题。' },
+      { name: '3DM', href: 'https://www.3dmgame.com/', description: '3DM游戏网为玩家提供最新的游戏新闻、攻略、单机游戏资源、汉化资源、游戏补丁、游戏论坛等,经过多年努力已成为游戏玩家首要选择的游戏资讯、游戏资源网站。' },
+      { name: '杉果游戏', href: 'https://www.sonkwo.cn/', description: '全方位正版游戏服务平台,提供正版低价游戏激活码,游戏预售、游戏促销折扣、免费游戏、游戏周边等游戏激活及周边购买服务,还有好玩的游戏推荐、游戏评测、游戏新闻资...' },
+      { name: 'NGA', href: 'https://bbs.nga.cn/', description: '艾泽拉斯国家地理ヽ(ﾟ∀ﾟ)ﾒ(ﾟ∀ﾟ)ﾉ ' },
+    ]
+  },
+  {
+    name: '游戏手册',
+    src: 'gamemanual',
+    hao: [
+      { name: 'FGO手册', href: 'http://fgo.vgtime.com/', description: '《Fate/Grand Order》是由TYPE-MOON推出智能手机专用的角色扮演游戏。国服由B站代理，中文名为《命运-冠位指定》。' },
+      { name: 'FGOwiki', href: 'https://fgo.wiki/w/%E9%A6%96%E9%A1%B5', description: '欢迎来到Mooncell！' },
+      { name: 'DarkSouls', href: 'https://leminerva.github.io/Dark-Souls-Documents/index.html', description: '黑暗之魂系列文本与数据档案。' },
+    ]
+  },
+  {
+    name: '游戏加速',
+    subtitle: '除了uu自我体验还行，其他描述均来自网络',
+    src: 'gameaccelerator',
+    hao: [
+      { name: '网易uu', href: 'https://uu.163.com/', description: '优点：延迟低 对联通网络特化，稳定，缺点：月租，价格贵' },
+      { name: '雷神加速器', href: 'https://jiasu.nn.com/', description: '优点：可以暂停时间 便宜 新人还有优惠，较稳定，网络优化质量可以，缺点：稳定性一般' },
+      { name: '月轮加速器', href: 'https://www.yuelun.com/', description: '优点：延迟最好(ヽ(ﾟ∀ﾟ)ﾉ听说ヽ(ﾟ∀ﾟ)ﾉ )，具有多重加速套餐 可以按小时购买 也可以按月购买，缺点：未知（推荐免费试用自己体验）' },
+      { name: '腾讯加速器', href: 'https://jiasu.qq.com/', description: '优点：大厂加速器的优点ヾ(ｏ･ω･)ﾉ，缺点：大厂加速器的缺点ヾ(ｏ･ω･)ﾉ' },
+      { name: 'golink', href: 'https://www.golink.com/', description: '优点：免费，缺点：稳定性随缘' },
+      { name: '奇妙', href: 'https://www.golink.com/', description: '优点：免费，缺点：稳定性随缘' },
+      { name: '流星', href: 'https://www.liuxing.com/', description: '优点：免费，缺点：稳定性随缘-较差' },
+      { name: '猫灵', href: 'https://www.baidu.com/s?wd=%E7%8C%AB%E7%81%B5%E5%8A%A0%E9%80%9F%E5%99%A8', description: '优点：免费，缺点：无官网，稳定性？' },
+    ]
+  }
+]
+export default [
+  {
+    url: '/game',
+    type: 'get',
+    response: () => {
+      return { name: '游戏专题', src: 'game', tab: bbs_tab }
+    }
+  }
+]
