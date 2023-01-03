5647652092933e0029472206c2994d1d249988c1
Damon Chu <40749434+damonchugs@users.noreply.github.com>
2023-01-03 12:04:35 +0800

    添加影视

--- mock/data/video.js
+++ mock/data/video.js
@@ -1,42 +1,100 @@
 const video_tab = [
   {
-    name: '视频网站',
-    src: 'videoweb',
+    name: "视频网站",
+    src: "videoweb",
     hao: [
-      { name: 'Bilibili', href: 'https://www.bilibili.com/', description: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili' },
-      { name: 'AcFun', href: 'https://www.acfun.cn/', description: 'AcFun是国内首家弹幕视频网站，这里有全网独家动漫新番， 友好的弹幕氛围，有趣的UP主，好玩有科技感的虚拟偶像，年轻人都在用。' },
-      { name: '美剧星球', href: 'https://meijuxingqiu.net/', description: '比起看剧，小破球更想陪你一起看更广阔的世界！' },
-      { name: '奈菲影视', href: 'https://www.nfmovies.com/', description: '奈非copy' },
-      { name: '奈落影视', href: 'https://www.newfii.com/', description: '奈落影视' },
-      { name: '北川影视', href: 'https://i.bcwzg.com/', description: '北川影视' },
-      { name: '555电影', href: 'https://www.555dianying.cc/', description: '555电影DY' },
-    ]
+      {
+        name: "Bilibili",
+        href: "https://www.bilibili.com/",
+        description: "哔哩哔哩 (゜-゜)つロ 干杯~-bilibili",
+      },
+      {
+        name: "AcFun",
+        href: "https://www.acfun.cn/",
+        description:
+          "AcFun是国内首家弹幕视频网站，这里有全网独家动漫新番， 友好的弹幕氛围，有趣的UP主，好玩有科技感的虚拟偶像，年轻人都在用。",
+      },
+      {
+        name: "美剧星球",
+        href: "https://meijuxingqiu.net/",
+        description: "比起看剧，小破球更想陪你一起看更广阔的世界！",
+      },
+      {
+        name: "奈菲影视",
+        href: "https://www.nfmovies.com/",
+        description: "奈非copy",
+      },
+      {
+        name: "奈落影视",
+        href: "https://www.newfii.com/",
+        description: "奈落影视",
+      },
+      {
+        name: "北川影视",
+        href: "https://i.bcwzg.com/",
+        description: "北川影视",
+      },
+      {
+        name: "555电影",
+        href: "https://www.555dianying.cc/",
+        description: "555电影DY",
+      },
+      {
+        name: "VOFLIX HD",
+        href: "http://voflix.com/",
+        description: "VOFLIX HD",
+      },
+    ],
   },
   {
-    name: '电影下载',
-    src: 'download',
+    name: "电影下载",
+    src: "download",
     hao: [
-      { name: '迅雷下载', href: 'https://xunlei8.cc/', description: '迅雷下载' },
-      { name: '电影天堂吧', href: 'http://www.dytt8.net/', description: '电影天堂吧' },
-      { name: '电影天堂', href: 'http://www.dy2018.com/', description: '电影天堂' },
-      { name: '磁力熊', href: 'https://www.cilixiong.com/', description: '纯净的1080P高分电影磁力下载站' }
-    ]
+      {
+        name: "迅雷下载",
+        href: "https://xunlei8.cc/",
+        description: "迅雷下载",
+      },
+      {
+        name: "电影天堂吧",
+        href: "http://www.dytt8.net/",
+        description: "电影天堂吧",
+      },
+      {
+        name: "电影天堂",
+        href: "http://www.dy2018.com/",
+        description: "电影天堂",
+      },
+      {
+        name: "磁力熊",
+        href: "https://www.cilixiong.com/",
+        description: "纯净的1080P高分电影磁力下载站",
+      },
+    ],
   },
   {
-    name: '字幕组',
-    src: 'zimu',
+    name: "字幕组",
+    src: "zimu",
     hao: [
-      { name: '射手网', href: 'http://assrt.net/', description: '射手网（伪）' },
-      { name: 'Subhd', href: 'http://www.subhd.cc/', description: '影视资料字幕' },
-    ]
-  }
-]
+      {
+        name: "射手网",
+        href: "http://assrt.net/",
+        description: "射手网（伪）",
+      },
+      {
+        name: "Subhd",
+        href: "http://www.subhd.cc/",
+        description: "影视资料字幕",
+      },
+    ],
+  },
+];
 export default [
   {
-    url: '/video',
-    type: 'get',
+    url: "/video",
+    type: "get",
     response: () => {
-      return { name: '影视娱乐', src: 'video', tab: video_tab }
-    }
-  }
-]
+      return { name: "影视娱乐", src: "video", tab: video_tab };
+    },
+  },
+];
