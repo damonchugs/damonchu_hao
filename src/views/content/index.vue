5ba641254781655e6791f7e84e07754790e6c9c4
Damon Chu <40749434+damonchugs@users.noreply.github.com>
2023-01-12 13:31:44 +0800

    eslint规范化

--- src/views/content/index.vue
+++ src/views/content/index.vue
@@ -1,30 +1,42 @@
 <template>
   <div class="app-container">
-    <div v-for="(t, index) in hao_data" :key="`hao_tab_${index}`" class="hao_tab" :id="`hao_tab-${t.src}`">
-        <Divider orientation="left" style="border-color: #7cb305" dashed>
-          {{ t.name }}
-          <span style="font-size: 12px;">{{ t.subtitle ? ` - ${t.subtitle}` : '' }}</span>
-        </Divider>
+    <div
+      v-for="(t, index) in hao_data"
+      :key="`hao_tab_${index}`"
+      class="hao_tab"
+      :id="`hao_tab-${t.src}`"
+    >
+      <Divider orientation="left" style="border-color: #7cb305" dashed>
+        {{ t.name }}
+        <span style="font-size: 12px">{{
+          t.subtitle ? ` - ${t.subtitle}` : ""
+        }}</span>
+      </Divider>
 
-        <div class="href_tab_div">
-          <div v-for="(te, inv) in t.hao" :key="`href_${inv}`" class="href_tab" @click="gotoHref(te.href)">
-            <p>{{ te.name }}</p>
-            <p>{{ te.description }}</p>
-          </div>
+      <div class="href_tab_div">
+        <div
+          v-for="(te, inv) in t.hao"
+          :key="`href_${inv}`"
+          class="href_tab"
+          @click="gotoHref(te.href)"
+        >
+          <p>{{ te.name }}</p>
+          <p>{{ te.description }}</p>
         </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 // 引用组件
-import Divider from 'ant-design-vue/lib/divider'
-import "ant-design-vue/lib/divider/style/css"
+import Divider from "ant-design-vue/lib/divider";
+import "ant-design-vue/lib/divider/style/css";
 // 引用插件
-import { ref, computed, onMounted, watch } from 'vue'
-import { useStore } from 'vuex'
+import { ref, computed, onMounted, watch } from "vue";
+import { useStore } from "vuex";
 // 请求数据
-import axios from 'axios'
+import axios from "axios";
 
 const store = useStore();
 
@@ -35,33 +47,33 @@ const hao_data = ref([]);
 
 // 监听地址，刷新页面数据
 watch(pathName, (val, new_val) => {
-  if (new_val.trim().split('/')[0] !== val.trim().split('/')[0]) {
-    const src = route.value.find(t => t.open)?.src || null;
+  if (new_val.trim().split("/")[0] !== val.trim().split("/")[0]) {
+    const src = route.value.find((t) => t.open)?.src || null;
     if (src) {
       getTabData(src);
     }
   }
-})
+});
 
-onMounted(()=>{
+onMounted(() => {
   getTabData(subject.value);
-})
+});
 
 // 获取tab_data
 const getTabData = (url) => {
-  axios.get(`/mock/${url}`).then(response => {
+  axios.get(`/mock/${url}`).then((response) => {
     hao_data.value = response.data.tab;
-    
+
     // 保存上次浏览专题
-    store.dispatch('router/setSubject', url)
-    localStorage.setItem('dchaoStorage', url);
-  })
-}
+    store.dispatch("router/setSubject", url);
+    localStorage.setItem("dchaoStorage", url);
+  });
+};
 
 // 跳转链接
 const gotoHref = (src) => {
-  window.open(src)
-}
+  window.open(src);
+};
 </script>
 
 <style lang="scss" scoped>
