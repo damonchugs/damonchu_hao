1eeabeed893d503334eb2f5fce1d0f2ddb3e4006
Damon Chu <40749434+damonchugs@users.noreply.github.com>
2023-03-20 15:30:08 +0800

    变量化scss

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,22 @@
-const path = require('path')
+const path = require("path");
 function resolve(dir) {
-  return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
 
-const name = 'DamonChu_Hao' // page title
+const name = "DamonChu_Hao"; // page title
 
 module.exports = {
   //baseUrl从 Vue CLI 3.3 起已弃用，请使用publicPath。
-  publicPath: './',
-  outputDir: 'hao',//默认值为dist，构建输出目录，若打包时不改会覆盖之前版本
+  publicPath: "./",
+  outputDir: "hao", //默认值为dist，构建输出目录，若打包时不改会覆盖之前版本
   // assetsDir: 'assets',//默认为'',静态文件目录(css,img,js,fonts)
-  lintOnSave: false,//默认true，开启会降低执行效率，用途是eslint检测代码，可选值：true,false,'error'，当为true时检测代码，出现错误时提示信息会输出到命令行，不会编译失败；为'error',出现错误编译会失败
-  devServer:{
-    open: true,//服务开启后直接在浏览器中打开主页面
+  lintOnSave: false, //默认true，开启会降低执行效率，用途是eslint检测代码，可选值：true,false,'error'，当为true时检测代码，出现错误时提示信息会输出到命令行，不会编译失败；为'error',出现错误编译会失败
+  devServer: {
+    open: true, //服务开启后直接在浏览器中打开主页面
     // host: 'localhost',//主机名
-    port: 4369,//端口号
-    https: false,//是否开启https
-    hotOnly: true,//是否开启热模块更新
+    port: 4369, //端口号
+    https: false, //是否开启https
+    hotOnly: true, //是否开启热模块更新
     // proxy:{
     //   //配置跨域,可参见官方文档https://webpack.js.org/configuration/dev-server/#devserverproxy
     //   '/api':{
@@ -35,14 +35,21 @@ module.exports = {
     name: name,
     resolve: {
       alias: {
-        '@': resolve('src')
-      }
-    }
+        "@": resolve("src"),
+      },
+    },
   },
   pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: []
-    }
-  }
-}
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [],
+    },
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/styles/variables.scss";`,
+      },
+    },
+  },
+};
