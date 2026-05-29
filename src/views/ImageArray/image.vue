638109cf8708bb48c825529fcce4229f84af597b
Damon Chu <40749434+damonchugs@users.noreply.github.com>
2026-05-29 11:31:39 +0800

    漫画竖屏图片宽度占据100%

--- src/views/ImageArray/image.vue
+++ src/views/ImageArray/image.vue
@@ -95,7 +95,7 @@ watch(() => props.src, (n) => {
     top: 0;
     left: 0;
     width: 100%;
-    height: 100%;
+    height: auto;
     object-fit: contain;
     transition: opacity 0.3s ease-in-out;
   }
